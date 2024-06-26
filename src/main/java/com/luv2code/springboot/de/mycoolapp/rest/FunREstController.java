cdaab8a4dcf25410d91847cf034529043dcf651d
alinaberlin <alina.ghetler@gmail.com>
2024-06-26 11:32:24 +0200

    Add costumise properties in and inject it into SpringBoot app

--- src/main/java/com/luv2code/springboot/de/mycoolapp/rest/FunREstController.java
+++ src/main/java/com/luv2code/springboot/de/mycoolapp/rest/FunREstController.java
@@ -1,11 +1,14 @@
 package com.luv2code.springboot.de.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunREstController {
     //expose "/"return ""Hello World"
+    @Value("${coach.name}")
+    private String coachName;
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
