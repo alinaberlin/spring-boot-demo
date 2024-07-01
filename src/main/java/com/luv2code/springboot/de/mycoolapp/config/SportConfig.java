f938fe9d934821260e09a9ebaf390adff7138e2f
alinaberlin <alina.ghetler@gmail.com>
2024-07-01 12:00:29 +0200

    define a bean method to configure the bean

--- src/main/java/com/luv2code/springboot/de/mycoolapp/config/SportConfig.java
+++ src/main/java/com/luv2code/springboot/de/mycoolapp/config/SportConfig.java
@@ -1,4 +1,15 @@
 package com.luv2code.springboot.de.mycoolapp.config;
 
+import com.luv2code.springboot.de.mycoolapp.common.Coach;
+import com.luv2code.springboot.de.mycoolapp.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SportConfig {
+    //define a bean method to configure the bean
+    @Bean
+    public Coach swimCoach(){
+        return new SwimCoach();
+    }
 }
